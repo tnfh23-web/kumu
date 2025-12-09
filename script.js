1a10c716f912c1bac31dcbbe56f4374d41ab7061
tnfh23-web <tnfh23@gmail.com>
2025-12-09 23:28:43 +0900

    반응형 수정

--- script.js
+++ script.js
@@ -11,8 +11,6 @@ $(".navi>li")
     $("#menu_bg").stop().fadeOut(300);
   });
 
-// ============================
-
 function mobiletopbar_init() {
   $(".sidebar-menu-button").click(function () {
     $(".sidebar-menu").addClass("active");
@@ -29,5 +27,29 @@ $(".dim-overlay").click(function () {
   $(".sidebar-menu").removeClass("active");
   $(".dim-overlay").removeClass("active");
 });
+$(".sidebar-menu-list > a").click(function (e) {
+  e.preventDefault();
 
+  const submenu = $(this).next(".sidebar-menu-list-1-sub");
+
+  $(".sidebar-menu-list-1-sub").not(submenu).slideUp();
+
+  submenu.slideToggle();
+});
+function plus_button_init() {
+  $(".plus-button").click(function () {
+    const ol = $(".last-text-box > ol");
+
+    ol.toggleClass("active");
+
+    // 버튼 텍스트 바꾸기
+    if (ol.hasClass("active")) {
+      $(this).text("접기");
+    } else {
+      $(this).text("더 보기");
+    }
+  });
+}
+
+plus_button_init();
 mobiletopbar_init();
