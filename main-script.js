40fadf61f169c2ff392e013374ee161d93aad70e
tnfh23-web <tnfh23@gmail.com>
2025-12-15 13:58:00 +0900

    main

--- main-script.js
+++ main-script.js
@@ -56,4 +56,36 @@ var swiper = new Swiper(".section-1-swiper", {
 });
 
 // 섹션 1 스와이퍼 끝
+
+// 섹션 2 시작
+function section3_toggle_init() {
+  $(".section-3 .arrow-box a").on("click", function (e) {
+    e.preventDefault();
+
+    const $section = $(this).closest(".section-3-box");
+    const $current = $section.find(".right-content.is-active");
+    const $next = $current.hasClass("right-content-1") ? $section.find(".right-content-2") : $section.find(".right-content-1");
+
+    $current.removeClass("is-active");
+    $next.addClass("is-active");
+  });
+}
+
+section3_toggle_init();
+
+// 섹션 2 끝
+
+// 디바이더 1 마퀴 시작
+
+document.querySelectorAll(".text-marquee-track").forEach((track) => {
+  track.innerHTML += track.innerHTML;
+});
+
+// 디바이더 1 마퀴 끝
+// 섹션 5 마퀴 시작
+
+const track = document.querySelector(".marquee-track");
+track.innerHTML += track.innerHTML;
+
+// /섹션 5 마퀴 끝
 mobiletopbar_init();
