3e447db62f557a0cb79b45d6dd1d15ee354caac0
tnfh23-web <tnfh23@gmail.com>
2025-12-17 15:28:39 +0900

    sec-2

--- main-script.js
+++ main-script.js
@@ -56,8 +56,78 @@ var swiper = new Swiper(".section-1-swiper", {
 });
 
 // 섹션 1 스와이퍼 끝
-
 // 섹션 2 시작
+
+function Box1__init() {
+  $(".tab_menu > ul > li").click(function (e) {
+    e.preventDefault();
+
+    let $this = $(this);
+    let thisIndex = $this.index();
+
+    $this.siblings(".active").removeClass("active");
+    $this.addClass("active");
+
+    let newTitle = $this.data("title");
+    let newDesc = $this.data("desc");
+    $("#sub_page_title").text(newTitle);
+    $("#sub_page_desc").text(newDesc);
+
+    let $sub_prod = $this.closest(".sub_prod");
+    let $productContainer = $sub_prod.find(".product_container");
+    console.log("active ", $sub_prod);
+
+    $productContainer.find(".prod_wrap.active").removeClass("active");
+    $productContainer.find(".prod_wrap").eq(thisIndex).addClass("active");
+  });
+}
+
+Box1__init();
+
+$(".tab_btn li").on("click", function (e) {
+  e.preventDefault();
+
+  const idx = $(this).index();
+
+  // 탭 active
+  $(".tab_btn li").removeClass("active");
+  $(this).addClass("active");
+
+  // 스와이퍼 전환
+  $(".section-2-swiper-wrap").removeClass("active").eq(idx).addClass("active");
+});
+new Swiper(".drink-swiper", {
+  loop: true,
+  slidesPerView: 4,
+  spaceBetween: 24,
+  navigation: {
+    nextEl: ".drink-swiper .swiper-button-next",
+    prevEl: ".drink-swiper .swiper-button-prev",
+  },
+});
+
+new Swiper(".bakery-swiper", {
+  loop: true,
+  slidesPerView: 4,
+  spaceBetween: 24,
+  navigation: {
+    nextEl: ".bakery-swiper .swiper-button-next",
+    prevEl: ".bakery-swiper .swiper-button-prev",
+  },
+});
+
+new Swiper(".goods-swiper", {
+  loop: true,
+  slidesPerView: 4,
+  spaceBetween: 24,
+  navigation: {
+    nextEl: ".goods-swiper .swiper-button-next",
+    prevEl: ".goods-swiper .swiper-button-prev",
+  },
+});
+
+// 섹션 2 끝
+// 섹션 3 시작
 function section3_toggle_init() {
   $(".section-3 .arrow-box a").on("click", function (e) {
     e.preventDefault();
@@ -73,7 +143,7 @@ function section3_toggle_init() {
 
 section3_toggle_init();
 
-// 섹션 2 끝
+// 섹션 3 끝
 
 // 디바이더 1 마퀴 시작
 
