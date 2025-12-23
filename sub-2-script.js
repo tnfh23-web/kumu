15c3face0c6cca4304d533f1f7e2d3f8e996477d
tnfh23-web <tnfh23@gmail.com>
2025-12-23 15:06:10 +0900

    디테일 수정

--- sub-2-script.js
+++ sub-2-script.js
@@ -1,3 +1,45 @@
+$(".submenu").hide();
+$("#menu_bg").hide();
+
+// 헤더 시작
+$(".navi>li")
+  .mouseenter(function () {
+    $(".submenu").stop().fadeIn(300);
+    $("#menu_bg").stop().fadeIn(300);
+  })
+  .mouseleave(function () {
+    $(".submenu").stop().fadeOut(300);
+    $("#menu_bg").stop().fadeOut(300);
+  });
+
+function mobiletopbar_init() {
+  $(".sidebar-menu-button").click(function () {
+    $(".sidebar-menu").addClass("active");
+    $(".dim-overlay").addClass("active");
+  });
+}
+
+$(".sidebar-close").click(function () {
+  $(".sidebar-menu").removeClass("active");
+  $(".dim-overlay").removeClass("active");
+});
+
+$(".dim-overlay").click(function () {
+  $(".sidebar-menu").removeClass("active");
+  $(".dim-overlay").removeClass("active");
+});
+$(".sidebar-menu-list > a").click(function (e) {
+  e.preventDefault();
+
+  const submenu = $(this).next(".sidebar-menu-list-1-sub");
+
+  $(".sidebar-menu-list-1-sub").not(submenu).slideUp();
+
+  submenu.slideToggle();
+});
+
+// 헤더 끝
+
 function Box1__init() {
   $(".tab_menu > ul > li").click(function (e) {
     e.preventDefault();
